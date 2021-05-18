1190b52b0ca3cc6f76e4698c808d91a9f37c767a
YoungChulShin <go1323@gmail.com>
2021-05-18 13:36:52 +0900

    for문을 이용해서 반복 테스트 진행

--- UnitTests/text-refiner/index.test.js
+++ UnitTests/text-refiner/index.test.js
@@ -1,7 +1,23 @@
 const sut = require("./index");
 
-test('sut transforms "hello  world" to "hello world"', () => {
-    const actual = sut("hello  world");
-    expect(actual).toBe("hello world");
-});
+// test('sut transforms "hello  world" to "hello world"', () => {
+//     const actual = sut("hello  world");
+//     expect(actual).toBe("hello world");
+// });
 
+// test('sut transforms "hello   world" to "hello world"', () => {
+//     const actual = sut("hello   world");
+//     expect(actual).toBe("hello world");
+// });
+
+// test('sut transforms "hello    world" to "hello world"', () => {
+//     const actual = sut("hello    world");
+//     expect(actual).toBe("hello world");
+// });
+
+test('sut correctyly works', () => {
+    for (const source of ['hello  world', 'hello   world', 'hello    world']) {
+        const actual = sut(source);
+        expect(actual).toBe("hello world");
+    }
+});
