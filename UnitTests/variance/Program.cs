d173495fdfe258d32bb1efd4981f5d6a420a7a9f
YoungChulShin <go1323@gmail.com>
2021-05-28 18:32:23 +0900

    분산 코드 추가

---
+++ UnitTests/variance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace variance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = args.Length;
+
+            if (n == 0)
+            {
+                System.Console.WriteLine("입력된 데이터가 없습니다.");
+                return;
+            }
+
+            if (n == 1)
+            {
+                System.Console.WriteLine("데이터가 부족해 분산을 계산할 수 없습니다");
+                return;
+            }
+
+            double[] source = new double[n];
+            for (int i = 0; i < n; i++) {
+                source[i] = double.Parse(args[i]);
+            }   
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++) {
+                sum += source[i];
+            }
+
+            double mean = sum / n;
+
+            double sumOfSquares = 0.0;
+            for (int i = 0; i < n; i++) {
+                sumOfSquares += (source[i] - mean) * (source[i] - mean);
+            }
+
+            double variance = sumOfSquares / (n - 1);
+            System.Console.WriteLine($"분산: {variance}");   
+        }
+    }
+}
