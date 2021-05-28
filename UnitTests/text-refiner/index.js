d9d371f9c98e083dc4a7ce570726a853d8ce3980
YoungChulShin <go1323@gmail.com>
2021-05-28 13:57:00 +0900

    random parameter test

--- UnitTests/text-refiner/index.js
+++ UnitTests/text-refiner/index.js
@@ -1,5 +1,20 @@
-function refineText(s) {
-    return s.replace("    ", " ").replace("  ", " ");
+function refineText(s, options) {
+    s = s
+    .replace("     ", " ")
+    .replace("\t", " ")
+    .replace("  ", " ")
+    .replace("  ", " ")
+    .replace("  ", " ")
+    .replace("mockist", "*******")
+    .replace("purist", "******");
+
+    if (options) {
+        for (const bannedword of options.bannedwords) {
+            s = s.replace(bannedword, "*".repeat(bannedword.length));
+        }
+    }
+
+    return s;
 }
 
-module.exports = refineText
+module.exports = refineText;
--- UnitTests/text-refiner/index.test.js
+++ UnitTests/text-refiner/index.test.js
@@ -1,3 +1,4 @@
+const faker = require("faker");
 const sut = require("./index");
 
 // test('sut transforms "hello  world" to "hello world"', () => {
@@ -29,7 +30,42 @@ test.each`
     ${"hello  world"}   | ${"hello world"}
     ${"hello   world"}  | ${"hello world"}
     ${"hello    world"} | ${"hello world"}
+    ${"hello     world"} | ${"hello world"}
+    ${"hello      world"} | ${"hello world"}
+    ${"hello       world"} | ${"hello world"}
 `('sut transforms "$source" to "$expected"', ({source, expected}) => {
     const actual = sut(source);
     expect(actual).toBe(expected);
+});
+
+test.each`
+    source | expected
+    ${"hello\t world"} | ${"hello world"}
+    ${"hello \tworld"} | ${"hello world"}
+`('sut transforms "$source" that contains tab character to "$expected"', 
+({source, expected}) => {
+    const actual = sut(source)
+    expect(actual).toBe(expected);
+});
+
+test.each`
+    source | bannedwords | expected
+    ${"hello mockist"} | ${["mockist", "purist"]} | ${"hello *******"}
+    ${"hello purist"} | ${["mockist", "purist"]} | ${"hello ******"}
+`('sut transfroms "$source" to "$expected"', 
+({ source, bannedwords, expected}) => {
+    const actual = sut(source, { bannedwords });
+    expect(actual).toBe(expected);
+});
+
+// random parameter test
+describe('given banned word', () => {
+    const bannedword = faker.lorem.word();
+    const source = "hello " + bannedword;
+    const expected = "hello " + "*".repeat(bannedword.length);
+
+    test('${bannedword} when invoke sut then it returns ${expected}', () => {
+        const actual = sut(source, { bannedwords: [bannedword]});
+        expect(actual).toBe(expected);
+    })
 })
