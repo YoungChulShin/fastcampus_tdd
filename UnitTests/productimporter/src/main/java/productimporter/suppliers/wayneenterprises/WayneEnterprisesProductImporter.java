b5f2e78497e12592f0d50093d773d9c56ea6c949
YoungChulShin <go1323@gmail.com>
2021-08-21 22:57:19 +0900

    sut_projects_all_products

--- UnitTests/productimporter/src/main/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter.java
+++ UnitTests/productimporter/src/main/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter.java
@@ -1,5 +1,7 @@
 package productimporter.suppliers.wayneenterprises;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import productimporter.Product;
@@ -8,17 +10,21 @@ import productimporter.ProductImporter;
 public final class WayneEnterprisesProductImporter implements ProductImporter {
 
   private final WayneEnterprisesProductSource datasource;
-  private final WayneEnterprisesProductTranslator translator;
+//  private final WayneEnterprisesProductTranslator translator;
 
   public WayneEnterprisesProductImporter(WayneEnterprisesProductSource datasource) {
     this.datasource = datasource;
-    this.translator = new WayneEnterprisesProductTranslator();
+//    this.translator = new WayneEnterprisesProductTranslator();
   }
 
   @Override
   public Iterable<Product> fetchProducts() {
-    return StreamSupport.stream(datasource.fetchProducts().spliterator(), false)
-        .map(translator::translateProduct)
-        .collect(Collectors.toList());
+//    return StreamSupport.stream(datasource.fetchProducts().spliterator(), false)
+//        .map(translator::translateProduct)
+//        .collect(Collectors.toList());
+
+    ArrayList<WayneEnterprisesProduct> products = new ArrayList<>();
+    datasource.fetchProducts().forEach(products::add);
+    return Arrays.asList(new Product[products.size()]);
   }
 }
---
+++ UnitTests/productimporter/src/test/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporte_specs.java
@@ -0,0 +1,20 @@
+package productimporter.suppliers.wayneenterprises;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import productimporter.DomainArgumentsSource;
+import productimporter.Product;
+
+public class WayneEnterprisesProductImporte_specs {
+
+  @ParameterizedTest
+  @DomainArgumentsSource
+  void sut_projects_all_products(WayneEnterprisesProduct[] source) {
+    var stub = new WayneEnterprisesProductSourceStub(source);
+    var sut = new WayneEnterprisesProductImporter(stub);
+
+    Iterable<Product> actual = sut.fetchProducts();
+
+    Assertions.assertThat(actual).hasSize(source.length);
+  }
+}
