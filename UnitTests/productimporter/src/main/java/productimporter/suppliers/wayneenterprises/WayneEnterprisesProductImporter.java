b9d6a6dbecbb9ca357725409a1c9285ad4d7fa85
YoungChulShin <go1323@gmail.com>
2021-08-22 21:29:01 +0900

    refactoring

--- UnitTests/productimporter/src/main/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter.java
+++ UnitTests/productimporter/src/main/java/productimporter/suppliers/wayneenterprises/WayneEnterprisesProductImporter.java
@@ -1,36 +1,24 @@
 package productimporter.suppliers.wayneenterprises;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-import productimporter.Pricing;
 import productimporter.Product;
 import productimporter.ProductImporter;
 
 public final class WayneEnterprisesProductImporter implements ProductImporter {
 
   private final WayneEnterprisesProductSource datasource;
-//  private final WayneEnterprisesProductTranslator translator;
+  private final WayneEnterprisesProductTranslator translator;
 
   public WayneEnterprisesProductImporter(WayneEnterprisesProductSource datasource) {
     this.datasource = datasource;
-//    this.translator = new WayneEnterprisesProductTranslator();
+    this.translator = new WayneEnterprisesProductTranslator();
   }
 
   @Override
   public Iterable<Product> fetchProducts() {
-//    return StreamSupport.stream(datasource.fetchProducts().spliterator(), false)
-//        .map(translator::translateProduct)
-//        .collect(Collectors.toList());
-
     return StreamSupport.stream(datasource.fetchProducts().spliterator(), false)
-        .map(x -> new Product(
-            "WAYNE",
-            x.getId(),
-            x.getTitle(),
-            new Pricing(new BigDecimal(x.getListPrice()), new BigDecimal(x.getListPrice() - x.getSellingPrice()))))
+        .map(translator::translateProduct)
         .collect(Collectors.toList());
   }
 }
